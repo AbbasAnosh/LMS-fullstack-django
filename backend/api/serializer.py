4eeff8d74c478783a8ffaace8e5fc788c1b5fb34
Abbas Anosh <abbasanosh123@gmail.com>
2024-07-03 20:24:35 -0600

    adding jwt to the userauth

--- backend/api/serializer.py
+++ backend/api/serializer.py
@@ -1,16 +1,27 @@
 from rest_framework import serializers
+# from rest_framework_simplejwt.tokens import Token
 from userauths.models import User, Profile
+from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
 
+
+
+class MyTokenObtainPairSerializer(TokenObtainPairSerializer):
+    @classmethod
+    def get_token(cls, user):
+        token = super().get_token(user)
+        token['username'] = user.username
+        token['full_name'] = user.full_name
+        token['email'] = user.email
+        
+        return token
 
 class UserSerializer(serializers.ModelSerializer):
-    
     class Meta:
         model = User
         fields = '__all__'
 
 
-class ProfileSerializer(serializers.ModelSerializer):
-    
+class ProfileSerializer(serializers.ModelSerializer):    
     class Meta:
         model = Profile
         fields = '__all__'
